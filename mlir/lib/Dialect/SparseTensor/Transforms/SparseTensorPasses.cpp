da6dcc6dbefb33e05c14f9ff46e6bd8d248badf5
Miheer Vaidya <m.vaidya@utah.edu>
2024-03-04 23:14:10 -0700

    add emitc to dependent dialects
    add emitc to SparseKokkosCodegen's dependent dialects.

--- mlir/lib/Dialect/SparseTensor/Transforms/SparseTensorPasses.cpp
+++ mlir/lib/Dialect/SparseTensor/Transforms/SparseTensorPasses.cpp
@@ -10,6 +10,7 @@
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Bufferization/IR/Bufferization.h"
 #include "mlir/Dialect/Complex/IR/Complex.h"
+#include "mlir/Dialect/EmitC/IR/EmitC.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/Func/Transforms/FuncConversions.h"
 #include "mlir/Dialect/GPU/IR/GPUDialect.h"
