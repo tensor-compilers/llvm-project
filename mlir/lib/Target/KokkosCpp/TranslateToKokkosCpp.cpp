127e50cb49fa82d2e38e5656c5fcb026e24ccab6
Miheer Vaidya <v.miheer@gmail.com>
2024-05-12 01:50:57 -0600

    add krs function

--- mlir/lib/Target/KokkosCpp/TranslateToKokkosCpp.cpp
+++ mlir/lib/Target/KokkosCpp/TranslateToKokkosCpp.cpp
@@ -2476,7 +2476,7 @@ void KokkosCppEmitter::populateSparseSupportFunctions()
         "sparseValuesI64"}) {
     registerCIface(true, funcName);
   }
-  for (std::string funcName : {"getSlice"}) {
+  for (std::string funcName : {"getSlice", "krs_getRank"}) {
     registerCIface(false, funcName);
   }
   registerCIface(false, "lexInsertI8");
